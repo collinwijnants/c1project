16120f5f6078e206e1e930cc1144214321815573
collinwijnants <collinwijnants@outlook.com>
2022-09-20 09:57:02 +0200

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head> 
+<link rel="stylesheet" href="app.css">
+</head>
+<header>
+    <img src="curio-01-zwart-logo-rgb.png" width="100" height="50">
+<body>
+
+
+</body>
+
+</html>
