7b9a773ffde18e65661aa641a1cf7b5e1d9b33a7
collinwijnants <collinwijnants@outlook.com>
2022-09-23 15:37:43 +0200

    kleuren aangepast

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,9 +5,19 @@
 </head>
 <header>
     <img src="curio-01-zwart-logo-rgb.png" width="100" height="50">
+    <div class="nav">
+        <a href="#infoText"> Info over blok</a>
+        <a href="#vakkenRooster"> Vakken/Rooster</a>
+    </div>
+</header>
 <body>
-
-
+<div class="vakkenrooster" id="vakkenRooster">
+    <img src="vakken.png" width="650" height="350">
+    <img src="rooster.png" width="650" height="350">
+</div>
+<div class="infoText">
+    <p id="infoText">Lorem ipsum, dolor sit amet consectetur adipisicing elit. At itaque velit aperiam veniam voluptates voluptatum aliquid iusto id odio tenetur pariatur alias quae corrupti nam minus, perspiciatis numquam, ex hic nihil, officiis recusandae veritatis. Inventore labore, consequatur vero aliquam beatae maiores illo, fugiat neque molestias autem corporis! Harum quia, assumenda iusto laborum sint voluptatem beatae nulla qui, hic sequi nemo molestias soluta vero necessitatibus rerum officiis! Doloremque dignissimos quibusdam voluptatum nulla enim aliquid. Asperiores architecto expedita vitae provident accusantium nemo, natus hic, exercitationem nesciunt, recusandae libero praesentium officia modi cumque eius molestiae sint dolorum perferendis consectetur commodi nulla officiis soluta fugit. Fuga eum reprehenderit distinctio quisquam, eius quod minus perspiciatis voluptate sed ad excepturi laboriosam itaque porro ratione error, aut cumque? Eaque deleniti et nemo ullam quo blanditiis. Ipsum ipsa ex numquam deserunt corrupti laborum quisquam facere maiores. Ad nemo earum tempora tempore illo autem nesciunt! Temporibus ullam reprehenderit quas distinctio quam vel repellendus quos adipisci ut fuga, in possimus dolores alias quidem non facere maiores optio officia, animi laborum explicabo quasi! Nihil, nemo! Et repudiandae laudantium ducimus eos possimus suscipit veritatis voluptas nesciunt tempora sit maiores unde, dolores quos nostrum recusandae, nemo quis repellat facere. Maxime harum autem minima voluptatibus suscipit rem, sit, ab vero, iusto labore consequatur expedita quo perferendis voluptatem culpa rerum fugit vitae beatae blanditiis molestias assumenda? Consequuntur fugiat suscipit ad repudiandae unde quam nesciunt, eveniet assumenda! Amet non culpa voluptates enim tempora labore accusantium deleniti sint voluptatem veritatis, animi excepturi id, placeat velit quos dignissimos. Dignissimos quidem ipsa aut rem, recusandae at inventore iure, totam alias voluptatem iusto eveniet dolorem saepe placeat, unde accusamus rerum officia. Repudiandae ab culpa expedita soluta, illum et iusto, suscipit tempora aliquid vitae omnis optio mollitia aspernatur, consequatur sunt molestias odio nulla obcaecati! Repellendus, sit molestias? Ex fuga ipsam rerum?</p>
+</div>
 </body>
 
 </html>
